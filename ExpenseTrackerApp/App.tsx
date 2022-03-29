e5148b0d51aa7d5526ff089e01a988fe1817d955
--global <--global>
2022-03-29 21:46:40 +0800

    After a successful login, a user will now be redirected to dashboard view

--- ExpenseTrackerApp/App.tsx
+++ ExpenseTrackerApp/App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from '@screens/Login';
 import Register from '@screens/Register';
+import Expenses from '@screens/Expenses';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +16,7 @@ const App = () => {
       >
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Expenses" component={Expenses} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ ExpenseTrackerApp/src/screens/Expenses/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+const Expenses = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Expenses View</Text>
+    </View>
+  );
+}
+
+export default Expenses;
