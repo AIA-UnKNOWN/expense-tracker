306d94b48b1530e6601f16762ceb27c324abc04b
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-04-12 21:13:39 +0800

    increased the security of spend api

--- server/controllers/AccountBalanceController.js
+++ server/controllers/AccountBalanceController.js
@@ -1,4 +1,5 @@
 const connection = require('@config/database');
+const jwt = require('jsonwebtoken');
 
 class AccountBalanceController {
 
@@ -7,27 +8,36 @@ class AccountBalanceController {
     this.topup = this.topup.bind(this);
   }
 
+  getCurrentUser(req, callback) {
+    jwt.verify(req.token, process.env.SECRET_KEY, (error, data) => {
+      if (error) return res.status(401).json(error);
+      callback(data.user);
+    });
+  }
+
   spend(req, res) {
-    const item = req.body;
-    connection.query('SELECT * FROM Users WHERE id = ?', item.user_id, (err, result) => {
-      if (err) throw err;
-      if (result.length === 0) return res.status(401).json({
-        message: 'Invalid username or password'
-      });
-      const user = result[0];
-      connection.query(
-        'UPDATE Users SET account_balance = ? WHERE id = ?',
-        [user.account_balance - item.amount, user.id],
-        (err, result) => {
-          if (err) throw err;
-          if (result.affectedRows > 0) {
-            connection.query('INSERT INTO History SET ?', item, (err, result) => {
-              if (err) throw err;
-              res.sendStatus(201);
-            });
+    this.getCurrentUser(req, user => {
+      const item = req.body;
+      connection.query('SELECT * FROM Users WHERE id = ?', user.id, (err, result) => {
+        if (err) throw err;
+        if (result.length === 0) return res.status(401).json({
+          message: 'Invalid username or password'
+        });
+        const queriedUser = result[0];
+        connection.query(
+          'UPDATE Users SET account_balance = ? WHERE id = ?',
+          [queriedUser.account_balance - item.amount, user.id],
+          (err, result) => {
+            if (err) throw err;
+            if (result.affectedRows > 0) {
+              connection.query('INSERT INTO History SET ?', { ...item, user_id: user.id }, (err, result) => {
+                if (err) throw err;
+                res.sendStatus(201);
+              });
+            }
           }
-        }
-      );
+        );
+      });
     });
   }
 
