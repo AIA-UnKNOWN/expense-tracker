e439ff04f34654507555db8933e3e4f16a1edd54
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-04-12 21:16:25 +0800

    increased the security for topup api

--- server/controllers/AccountBalanceController.js
+++ server/controllers/AccountBalanceController.js
@@ -42,30 +42,32 @@ class AccountBalanceController {
   }
 
   topup(req, res) {
-    const transaction = req.body;
-    connection.query('SELECT * FROM Users WHERE id = ?', transaction.user_id, (err, result) => {
-      if (err) throw err;
-      if (result.length === 0) return res.status(401).json({
-        message: 'Invalid username or password'
-      });
-      const user = result[0];
-      connection.query(
-        'UPDATE Users SET account_balance = ? WHERE id = ?',
-        [user.account_balance + transaction.amount, user.id],
-        (err, result) => {
-          if (err) throw err;
-          connection.query(
-            'INSERT INTO History SET ?',
-            { ...transaction, label: 'TOPUP' },
-            (err, result) => {
-              if (err) throw err;
-              if (result.affectedRows > 0) {
-                res.json(201);
+    this.getCurrentUser(req, user => {
+      const transaction = req.body;
+      connection.query('SELECT * FROM Users WHERE id = ?', user.id, (err, result) => {
+        if (err) throw err;
+        if (result.length === 0) return res.status(401).json({
+          message: 'Invalid username or password'
+        });
+        const queriedUser = result[0];
+        connection.query(
+          'UPDATE Users SET account_balance = ? WHERE id = ?',
+          [queriedUser.account_balance + transaction.amount, user.id],
+          (err, result) => {
+            if (err) throw err;
+            connection.query(
+              'INSERT INTO History SET ?',
+              { ...transaction, label: 'TOPUP', user_id: user.id },
+              (err, result) => {
+                if (err) throw err;
+                if (result.affectedRows > 0) {
+                  res.json(201);
+                }
               }
-            }
-          );
-        }
-      );
+            );
+          }
+        );
+      });
     });
   }
 
