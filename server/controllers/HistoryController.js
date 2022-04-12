477ec1b7c43969fd62eec3fc6fab9208244f6df7
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-04-12 20:45:03 +0800

    implemented HistoryController boilerplate

---
+++ server/controllers/HistoryController.js
@@ -0,0 +1,18 @@
+class HistoryController {
+
+  constructor() {
+    this.getExpensesHistory = this.getExpensesHistory.bind(this);
+    this.getTopupHistory = this.getTopupHistory.bind(this);
+  }
+
+  getExpensesHistory(req, res) {
+    // Get all the expenses history
+  }
+
+  getTopupHistory(req, res) {
+    // Get all the topup history
+  }
+
+}
+
+export default new HistoryController;
