d598af3b07f9c9fe8278a504a526bf7f35fcfb59
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-04-15 04:33:59 +0800

    user can now do a topup for their account

--- ExpenseTrackerApp/src/common/ExpenseTopup/Navigations/Topup/hook.ts
+++ ExpenseTrackerApp/src/common/ExpenseTopup/Navigations/Topup/hook.ts
@@ -1,10 +1,46 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import * as Keychain from 'react-native-keychain';
+import api from '@api';
+import useHistories from '@screens/Expenses/Histories/hook';
 
 const useTopup = () => {
+  const isMounted = useRef(true);
+  const { getTopupsHistory } = useHistories();
   const [amount, setAmount] = useState("0");
 
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    }
+  }, []);
+
+  const getUserToken = async () => {
+    try {
+      const credentials = await Keychain.getGenericPassword();
+      const token = credentials.password;
+      if (token) return token;
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
   const topup = async () => {
-    console.log({ amount: parseInt(amount) })
+    const token = await getUserToken();
+    try {
+      const response = await fetch(`${api}/account-balance/topup`, {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ amount: parseInt(amount) })
+      });
+      if (!isMounted.current) return;
+      getTopupsHistory();
+    } catch(error) {
+      console.log(error);
+    }
   }
 
   return { amount, setAmount, topup }
